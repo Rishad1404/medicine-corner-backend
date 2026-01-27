439e943959b1a1704b56fc4e398b4264cb033d64
Rishad1404 <rishadislam2019@gmail.com>
2026-01-27 21:21:34 +0600

    user schema added with better auth

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import express, { Application } from "express";
 import { categoryRouter } from "./modules/category/category.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors"
 
 const app:Application=express();
+
+app.use(cors({
+    origin:process.env.APP_URL || "http://localhost:4000"
+}))
+
+app.all("/api/auth/*splat",toNodeHandler(auth))
+
 app.use(express.json())
 
 app.use("/api/categories",categoryRouter);
